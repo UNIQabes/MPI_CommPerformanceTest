7aef125b3cf9c3774d53bea009fe9197f780bbe6
UNIQabes <30741110+UNIQabes@users.noreply.github.com>
2024-04-29 18:06:57 +0900

    ちょっと修正

--- comm_time_eval.sh
+++ comm_time_eval.sh
@@ -4,7 +4,7 @@ aryizes=(50000 100000 150000 200000 250000)
 
 iterC=10
 
-echo "\"P2P\", \"AllGather\""
+echo "\"ThreadN\",\"P2P\", \"AllGather\""
 for arysize in $aryizes
 do
 	sum_p2p=0
@@ -14,10 +14,10 @@ do
 		a=$(mpirun -np 2 bin/p2p_time $arysize)
 		sum_p2p=$((a+sum_p2p))
 
-		a=$(mpirun -np 2 bin/allgather_time $arysize)
+		a=$(mpirun -np 8 bin/allgather_time $arysize)
 		sum_allgather=$((a+sum_allgather))
 	done
 	sum_p2p=$((sum_p2p/iterC))
 	sum_allgather=$((sum_allgather/iterC))
-	echo "\"$sum_p2p\", \"$sum_allgather\""
+	echo "\"$arysize\",\"$sum_p2p\", \"$sum_allgather\""
 done
