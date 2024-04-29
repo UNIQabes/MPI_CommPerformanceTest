561e6e5c2a8b04e49977c488cb0d0072c29983f9
UNIQabes <30741110+UNIQabes@users.noreply.github.com>
2024-04-29 16:18:56 +0900

    コマンドライン引数でデータ数を設定できるようにした

--- p2p_time.c
+++ p2p_time.c
@@ -1,28 +1,41 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "second.c"
-
-const int DATANUM = 10000;
 
 int main(int argc, char *argv[])
 {
-	int buf[DATANUM];
+	int DataNum = 0;
+	if (argc == 2)
+	{
+		DataNum = atoi(argv[1]);
+	}
+	else
+	{
+		fprintf(stderr, "引数が1つではありません\n");
+	}
+
+	int *buf = malloc(sizeof(int) * DataNum);
 	MPI_Status status;
 	int rank;
 
 	MPI_Init(&argc, &argv);
 	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+	if (rank == 0)
+	{
+		// printf("DataNum:%d\n", DataNum);
+	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
 	double start = second();
 
 	if (rank == 0)
 	{
-		MPI_Send(buf, DATANUM, MPI_INT, 1, 2024, MPI_COMM_WORLD);
+		MPI_Send(buf, DataNum, MPI_INT, 1, 2024, MPI_COMM_WORLD);
 	}
 	else if (rank == 1)
 	{
-		MPI_Recv(buf, DATANUM, MPI_INT, 0, 2024, MPI_COMM_WORLD, &status);
+		MPI_Recv(buf, DataNum, MPI_INT, 0, 2024, MPI_COMM_WORLD, &status);
 	}
 
 	MPI_Barrier(MPI_COMM_WORLD);
